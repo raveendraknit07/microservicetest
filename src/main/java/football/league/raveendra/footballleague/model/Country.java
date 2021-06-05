8857f67d9748bf0c8b0a31fce91ee0d87b7e6071
raveendrapsl <raveendraknit@gmail.com>
2021-06-05 17:40:22 +0530

    hello chekc test2

--- src/main/java/football/league/raveendra/footballleague/model/Country.java
+++ src/main/java/football/league/raveendra/footballleague/model/Country.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
-public class Country {
+public class Countryaaaa {
   @JsonProperty("country_id")
   private int id;
   @JsonProperty("country_name")
